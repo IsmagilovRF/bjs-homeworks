8ab1e5ce987557e81604baa81404b39579873850
IsmagilovRF <ledohod@icloud.com>
2020-06-23 14:16:05 +0300

    Second Edit

--- 1.2-extended-syntax/task.js
+++ 1.2-extended-syntax/task.js
@@ -12,50 +12,42 @@ function getResult(a,b,c){
         return x; 
     }
     else {
-        x[0] = ( -b  ) / ( 2 * a );
-        x[1] = ( -b  ) / ( 2 * a );
-        return x;
+        return ( -b  ) / ( 2 * a );
     }
 }
 
 function getAverageMark(marks){
     // код для задачи №2 писать здесь
-    //console.log(marks.length);
+    const setNumberOfRatings = 5;
     if (marks.length === 0) {
         console.log('Оценок нет и массив пустой. Выводим: ' + marks.length);
-        return averageMark;
+        return 0;
+    } 
+    if (marks.length > setNumberOfRatings) {
+        console.log(`Количество оценок оказалось больше ${setNumberOfRatings}, используем ${setNumberOfRatings} первых оценок из ${marks.length}:`);
+        marks = marks.slice(0,setNumberOfRatings);
+        console.log('Используемые оценки в массиве result = ' + marks);
     }
-    let numberOfRatings = marks.length > 5 ? 5 : marks.length;
+    else {
+        console.log(`Количество оценок оказалось не больше ${setNumberOfRatings}, испльзуем все оценки (${marks.length} оценок)`);
+    }
     console.log('Количество оценок всего = ' + marks.length, marks);
-    let  textNumberOfRatings = marks.length > 5 ? 
-    'Количество оценок оказалось больше 5, используем пять первых оценок из ' : 
-    'Количество оценок оказалось не больше 5, испльзуем все оценки';
-    console.log(textNumberOfRatings + ' ' + marks.length);
-    let result = marks.slice(0,numberOfRatings).reduce(function(sum, current) {
-        return sum + current;
-    }, 0);
-    console.log( 'Cумма для ' + numberOfRatings + ' оценок = ' + result );
-    console.log( 'Cредняя из ' + numberOfRatings + ' оценок = ' + result / numberOfRatings );
-    // среднее значение для всех оценок больше пяти
-    if (marks.length > 5) {
-        result = marks.slice(0,marks.length).reduce(function(sum, current) {
-            return sum + current;
-        }, 0);
-        console.log( `Cредняя оценка (с учетом всех ${marks.length} оценок) = ` + result / marks.length );
+    let arraySum = function (array){
+        let s = 0;
+        for (let i = 0; i < array.length; i++) {
+            s += array[i];
+        }
+        return s;
     }
-    return result = result / marks.length; // это среднее с учетом всех оценок, а не только пяти первых
+    console.log('Сумма оценок = ' + arraySum(marks));
+    console.log(`Cредняя оценка (с учетом всех ${marks.length} оценок) = ` + arraySum(marks) / marks.length );
+    return arraySum(marks) / marks.length;
 }
 
 function askDrink(name,dateOfBirthday){
-    // код для задачи №3 писать здесь
-    //console.log('сегодня: ' + new Date().getFullYear());
-    //console.log('дата рождения перца: ' + dateOfBirthday.getFullYear());
-    // возраст перца:
     let age = new Date().getFullYear() - dateOfBirthday.getFullYear();
     console.log(age);
-    //return
-    return decisionText = age > 18 ? 
+    return age > 18 ? 
     `Не желаете ли олд-фэшн, ${name}?` : 
     `Сожалею, ${name}, но я не могу вам продать алкоголь. Зато могу предложить вам замечательный клюквенный компот!`;
-    // return result;
 }
